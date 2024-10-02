3a7b4efde838bdaeb996f4f1393e8f57fcda1503
Nico <nicolasalfonsoferreira (at) gmail.com>
2024-10-02 19:49:03 -0300

    added home_page tests

--- pages/tests.py
+++ pages/tests.py
@@ -1,3 +1,14 @@
-from django.test import TestCase
+from django.test import SimpleTestCase
+from django.urls import reverse
 
-# Create your tests here.
+# We import SimpleTestCase as the homepage does not rely on the database.
+# If it did indeed rely on the database (like the Bet History page does), we would use TestCase instead.
+class HomePageTests(SimpleTestCase):
+    def test_url_exists_at_correct_location_homepageview(self):
+        response = self.client.get("/")
+        self.assertEqual(response.status_code, 200)
+
+    def test_homepage_view(self):
+        response = self.client.get(reverse("home_page"))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "home_page.html")
