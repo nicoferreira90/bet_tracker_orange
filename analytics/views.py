58ec5503349aae58cd2d7765bbd3008307cf4d8f
nico <nicolasalfonsoferreira@gmail.com>
2024-10-22 18:45:53 -0300

    minor tweaks

--- analytics/views.py
+++ analytics/views.py
@@ -2,5 +2,6 @@ from django.shortcuts import render
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.views.generic import TemplateView
 
-class AnalyticsPageView(TemplateView):
+class AnalyticsPageView(LoginRequiredMixin, TemplateView):
     template_name = "analytics/analytics_page.html"
+    login_url = "/users/login/"
