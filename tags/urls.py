9db5deb08b63324f96da02803c0ab2ac2584e402
nico <nicolasalfonsoferreira@gmail.com>
2024-10-31 00:44:51 -0300

    more or less finished bet tag page

--- tags/urls.py
+++ tags/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import TagListView, NewTagView, UpdateTagView, DeleteTagView, BetTagPageView
+from .views import TagListView, NewTagView, UpdateTagView, DeleteTagView, BetTagPageView, BetNewTagView
 
 urlpatterns = [
     path("", TagListView.as_view(), name="tag_list"),
@@ -7,6 +7,6 @@ urlpatterns = [
     path("update_tag/<int:pk>/", UpdateTagView.as_view(), name="update_tag"),
     path("delete_tag/<int:pk>/", DeleteTagView.as_view(), name="delete_tag"),
     path("bet_tag_page/<int:pk>/", BetTagPageView.as_view(), name="bet_tag_page"),
-
+    path("bet_new_tag/", BetNewTagView.as_view(), name="bet_new_tag"),
 
 ]
--- tags/views.py
+++ tags/views.py
@@ -1,4 +1,3 @@
-from django.shortcuts import render
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.views.generic import ListView, CreateView, DeleteView, UpdateView, DetailView
 from django.urls import reverse_lazy
@@ -22,6 +21,8 @@ class NewTagView(LoginRequiredMixin, CreateView):
         # Set the owner of the bet to the current user
         form.instance.tag_owner = self.request.user
         return super().form_valid(form)
+
+    
     
 class UpdateTagView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     model = Tag
@@ -45,13 +46,59 @@ class DeleteTagView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
         """Checks if bet owner is the same as the current user."""
         obj = self.get_object()
         return obj.tag_owner == self.request.user
+
     
 class BetTagPageView(LoginRequiredMixin, UserPassesTestMixin, DetailView):
 
     model = Bet
     template_name = "tags/bet_tag_page.html"
 
+    def post(self, request, *args, **kwargs):
+        self.object = self.get_object()
+        context = self.get_context_data(**kwargs)
+
+        if request.POST.get('tag-select'):
+            chosen_tag = Tag.objects.get(label=request.POST.get('tag-select'))
+            chosen_tag.associated_bets.add(self.object)
+        else:
+            removed_tag = Tag.objects.get(id=request.POST.get('tag-id'))
+            removed_tag.associated_bets.remove(self.object)
+        context["bet"] = self.get_object()
+
+        return self.render_to_response(context)
+    
+    def get(self, request, *args, **kwargs):
+        self.object = self.get_object()
+        context = self.get_context_data(**kwargs)
+
+        return self.render_to_response(context)
+
+
+    def get_context_data(self, **kwargs): 
+        context = super().get_context_data(**kwargs)  
+        context["bet"] = self.get_object()
+        print("debug line")
+        print(context["bet"])
+        context["inactive_tags"] = Tag.objects.exclude(associated_bets=context["bet"]).distinct()
+        print(context["inactive_tags"])
+        context["pk"] = self.get_object().pk
+        print(context["pk"])
+        return context
+    
+
     def test_func(self):
         """Checks if bet owner is the same as the current user."""
         obj = self.get_object()
         return obj.bet_owner == self.request.user
+
+class BetNewTagView(LoginRequiredMixin, CreateView):
+    model = Tag
+    form_class = TagForm
+    template_name = "tags/new_tag.html"
+    login_url = "/users/login/"
+    success_url = reverse_lazy("bet_tag_page")
+
+    def form_valid(self, form):
+        # Set the owner of the bet to the current user
+        form.instance.tag_owner = self.request.user
+        return super().form_valid(form)
