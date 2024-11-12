0077d41a554630330964877c057cacb81d35e1cb
nico <nicolasalfonsoferreira@gmail.com>
2024-11-12 15:01:25 -0300

    changed admin panel url

--- README.md
+++ README.md
@@ -2,3 +2,8 @@
 12/11:
 
 App is finished. Need to decide if the About page will ask for donations, and write some further tests.
+
+TODO: 
+
+- get domain?
+- learn docker to deploy
--- bet_tracker/urls.py
+++ bet_tracker/urls.py
@@ -4,7 +4,7 @@ from django.urls import path, include
 from debug_toolbar.toolbar import debug_toolbar_urls
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('kbzon/', admin.site.urls),
     path('users/', include("users.urls") ),
     path('users/', include("django.contrib.auth.urls")),
     path('bets/', include("bets.urls")),
