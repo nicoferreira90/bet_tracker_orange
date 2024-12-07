0ac5b18eb764fe4e71a200a535b3a8d725fbfe11
Nico <nicolasalfonsoferreira (at) gmail.com>
2024-12-07 05:09:35 -0300

    AJAX apparently working well for bet deletion

--- bets/urls.py
+++ bets/urls.py
@@ -1,12 +1,17 @@
 
 from django.urls import path
-from .views import BetHistoryView, NewBetView, DeleteBetView, UpdateBetView
+from .views import BetHistoryView, NewBetView, UpdateBetView, delete_bet_view
 
 urlpatterns = [
     path("history/", BetHistoryView.as_view(), name="bet_history"),
     path("new_bet/", NewBetView.as_view(), name="new_bet"),
-    path("delete_bet/<uuid:pk>/", DeleteBetView.as_view(), name="delete_bet"),
     path("update_bet/<uuid:pk>/", UpdateBetView.as_view(), name="update_bet"),
     path("", BetHistoryView.as_view(), name="bet_history"),
 
 ]
+
+htmx_urlpatterns = [
+    path("delete_bet/<uuid:pk>/", delete_bet_view, name="delete_bet"),
+]
+
+urlpatterns += urlpatterns + htmx_urlpatterns
--- bets/views.py
+++ bets/views.py
@@ -1,8 +1,13 @@
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
-from django.views.generic import ListView, CreateView, DeleteView, UpdateView
+from django.views.generic import ListView, CreateView, UpdateView
+from django.views.decorators.http import require_http_methods
 from django.urls import reverse_lazy
+from django.shortcuts import render
 from .forms import BetForm
 from .models import Bet
+from django.core.exceptions import PermissionDenied
+from django.contrib.auth.decorators import login_required
+from django.core.paginator import Paginator
 
 
 class BetHistoryView(LoginRequiredMixin, ListView):
@@ -27,17 +32,6 @@ class NewBetView(LoginRequiredMixin, CreateView):
         form.instance.bet_owner = self.request.user
         return super().form_valid(form)
 
-class DeleteBetView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
-    model = Bet
-    template_name = "bets/delete_bet.html"
-    success_url = reverse_lazy("bet_history")
-    login_url = reverse_lazy("account_login")
-
-    def test_func(self):
-        """Checks if bet owner is the same as the current user."""
-        obj = self.get_object()
-        return obj.bet_owner == self.request.user
-
 class UpdateBetView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     model = Bet
     form_class = BetForm
@@ -50,5 +44,21 @@ class UpdateBetView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
         obj = self.get_object()
         return obj.bet_owner == self.request.user
 
+@require_http_methods(['DELETE'])
+@login_required
+def delete_bet_view(request, pk):
+    if request.user == Bet.objects.get(pk=pk).bet_owner:
+        Bet.objects.get(pk=pk).delete()
+        bet_list = Bet.objects.filter(bet_owner=request.user).order_by('-date_added')
 
+        paginator = Paginator(bet_list, 20)
+        page_obj = paginator.get_page(1)
+
+        context = {
+            "bet_list": bet_list,
+            "page_obj": page_obj,
+        }
+        return render(request, "bets/partials/bet_history_table.html", context=context)
+    else:
+        raise PermissionDenied
 
