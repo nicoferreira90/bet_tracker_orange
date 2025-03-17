22a0185fee8e77b9c1d1d3e4eadfbf13a919fdc6
Nico <nicolasalfonsoferreira (at) gmail.com>
2025-03-16 21:46:18 -0300

    added version history page, work in progress

--- pages/urls.py
+++ pages/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import HomePageView, AboutPageView, PasswordChangeSuccessView
+from .views import HomePageView, AboutPageView, PasswordChangeSuccessView, VersionHistoryPageView
 
 urlpatterns = [
     path("", HomePageView.as_view(), name="home_page"),
     path("about/", AboutPageView.as_view(), name="about_page"),
+    path("version_history/", VersionHistoryPageView.as_view(), name="version_history"),
     path(
         "accounts/password_change_success/",
         PasswordChangeSuccessView.as_view(),
