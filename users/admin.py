7464b7d1583a5b8422475d211d8b670ba46deb3c
nico <nicolasalfonsoferreira@gmail.com>
2025-04-09 15:55:29 -0400

    added # of bets to CustomUser admin panel

--- users/admin.py
+++ users/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from .forms import CustomUserCreationForm, CustomUserChangeForm
 from .models import CustomUser
+from bets.models import Bet
 
 
 class CustomUserAdmin(UserAdmin):
@@ -16,7 +17,15 @@ class CustomUserAdmin(UserAdmin):
         "odds_preference",
         "items_per_page",
         "remove_color_from_tables",
+        "get_bet_count",
     ]
+
+    def get_bet_count(self, obj):
+        return Bet.objects.filter(
+            bet_owner=obj
+        ).count()  # Count the number of Bet objects related to this user
+
+    get_bet_count.short_description = "Number of Bets"  # Set a custom column name
 
 
 admin.site.register(CustomUser, CustomUserAdmin)
